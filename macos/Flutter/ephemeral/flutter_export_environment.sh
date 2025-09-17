a7f3c8abacdccef94fef485ceef6b320983105d0
cyberpwn <dan@arcane.art>
2025-09-17 04:55:45 -0500

    f
    f

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\cyberpwn\Documents\development\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\cyberpwn\Documents\development\workspace\toxic"
+export "FLUTTER_ROOT=/Users/cyberpwn/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cyberpwn/development/workspace/cyberpwnn/dart_toxic"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.3.0"
-export "FLUTTER_BUILD_NUMBER=1.3.0"
+export "FLUTTER_BUILD_NAME=1.3.4"
+export "FLUTTER_BUILD_NUMBER=1.3.4"
+export "FLUTTER_CLI_BUILD_MODE=debug"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
